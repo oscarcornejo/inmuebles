aaa1e37d0104132a7d508ce6a7715f54fc707bf9
Oscar Cornejo <oscarcornejo10@gmail.com>
2020-04-02 13:01:32 -0300

    first commit

--- src/config/configFirebase.js
+++ src/config/configFirebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase/app';
 const firebaseConfig = {
-    apiKey: "AIzaSyD4-4idSn9OJUWpPfpQZ3h2S8KTE2UVHdM",
-    authDomain: "propiedades-app.firebaseapp.com",
-    databaseURL: "https://propiedades-app.firebaseio.com",
-    projectId: "propiedades-app",
-    storageBucket: "propiedades-app.appspot.com",
-    messagingSenderId: "207954204029",
-    appId: "1:207954204029:web:646fbb9461bb61a94fe814",
-    measurementId: "G-KY93VJ3GD8"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   firebase.initializeApp(firebaseConfig);
   export default firebase;
